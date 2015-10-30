4e163c42ccd714bfa4a7acc6555402a5d8c13067
Felix Angerborn <felixa@opera.com>
2015-10-30 22:08:16 +0100

    use continue instead of is_ok

--- project_euler/43/main.py
+++ project_euler/43/main.py
@@ -5,21 +5,19 @@ def m(l, n):
 
 ok_numbers = []
 for p in itertools.permutations(range(0,10)):
-    is_ok = True
     if m(p[1:4], 2):
-        is_ok = False
+        continue
     if m(p[2:5], 3):
-        is_ok = False
+        continue
     if m(p[3:6], 5):
-        is_ok = False
+        continue
     if m(p[4:7], 7):
-        is_ok = False
+        continue
     if m(p[5:8], 11):
-        is_ok = False
+        continue
     if m(p[6:9], 13):
-        is_ok = False
+        continue
     if m(p[7:10], 17):
-        is_ok = False
-    if is_ok:
-        ok_numbers.append(int("".join(map(str,p))))
+        continue
+    ok_numbers.append(int("".join(map(str,p))))
 print sum(ok_numbers)
