febc1a1f14de70ff56895fde44b2238d81a26d23
Felix Angerborn <felixa@opera.com>
2015-12-02 08:33:51 +0100

    advent of code day 2

---
+++ advent_of_code/day2.py
@@ -0,0 +1,19 @@
+def paper_for_dimensions(l, w, h):
+    shortest, middle, _ = sorted([l, w, h])
+    return 2*(l*w + l*h + w*h) + shortest*middle
+
+def ribbon_for_dimensions(l, w, h):
+    shortest, middle, _ = sorted([l, w, h])
+    smallest_perimeter = 2 * (shortest + middle)
+    volume = l * w * h
+    return smallest_perimeter + volume
+
+paper = 0
+ribbon = 0
+with open("day2.in", "r") as f:
+    for line in f.readlines():
+        l, w, h = map(int, line.split("x"))
+        paper += paper_for_dimensions(l, w, h)
+        ribbon += ribbon_for_dimensions(l, w, h)
+    print paper
+    print ribbon
